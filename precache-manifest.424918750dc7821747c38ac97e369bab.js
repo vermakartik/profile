4c878239be5289753027551d7a5135c577f4df19
Kartik Verma <kvs1297@gmail.com>
2019-06-13 10:34:08 +0530

    Updates

---
+++ precache-manifest.424918750dc7821747c38ac97e369bab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c06fee1fe991b4486bf4cbca7f1e3ff6",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "7410d3f726f603444429",
+    "url": "/profile/static/css/main.67e0e25d.chunk.css"
+  },
+  {
+    "revision": "4fa6cf9fe5e45244cc3e",
+    "url": "/profile/static/js/2.28c8f597.chunk.js"
+  },
+  {
+    "revision": "7410d3f726f603444429",
+    "url": "/profile/static/js/main.f155f19f.chunk.js"
+  },
+  {
+    "revision": "189fd871db148202566e",
+    "url": "/profile/static/js/runtime~main.cc8686f1.js"
+  },
+  {
+    "revision": "67446f52a9ae1acd40f4b6e551ab3f5f",
+    "url": "/profile/static/media/Kartik.67446f52.jpg"
+  },
+  {
+    "revision": "51ca9de654d5110a41570de2c9fb2d6f",
+    "url": "/profile/static/media/profile.51ca9de6.jpg"
+  }
+]);
